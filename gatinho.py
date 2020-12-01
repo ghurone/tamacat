c54987502834ac7ff9d37a4b2ec5307e7f031158
Ghuron <ghuron@usp.br>
2020-11-30 21:57:59 -0300

    Mudei um pouquinho :P

--- gatinho.py
+++ gatinho.py
@@ -8,13 +8,13 @@ class Gatinho:
         self.dormindo = False
         self.idade = 0
         
-        self.humores = ['feliz', 'triste', 'agitado', 'cansado', 'quieto', 'brincalhão', 'assustado']
+        self.humores = ['feliz', 'triste', 'agitado', 'cansado', 'quieto', 'brincalhão', 'assustado', 'irritado']
         
         self.humor = self.humores[randint(0, len(self.humores))]
         
     def comer(self): 
         if not self.dormindo:
-            # criar a classe comida sei lá
+            # criar a classe comida sei
             pass
         else:
             print(f'{self.nome} está dormindo...')
@@ -27,14 +27,21 @@ class Gatinho:
             print(f'{self.nome} está dormindo, acorde-o para brincar!')
     
     def dormir(self):
-        self.dormindo = True
-        print('zzZzZzzzz')
+        if not self.dormindo:
+            self.dormindo = True
+            print('zzZzZzzzz')
+        else:
+            print(f'{self.nome} já está dormindo!')
         
     def acordar(self):
-        self.dormindo = False
-        print(f'{self.nome} acordou!')
+        if self.dormindo:
+            self.dormindo = False
+            print(f'{self.nome} acordou!')
+        else:
+            print(f'{self.nome} já está acordado!')
     
-    def miar(self):
+    @staticmethod
+    def miar():
         print('Miau!')
 
     @staticmethod
@@ -42,8 +49,15 @@ class Gatinho:
         # :P
         print(':P')
 
+    @staticmethod
+    def ronronar():
+        print('rrrRRRRrrrRRr')
+
     def mostrar_humor(self):
         print(f'{self.nome} está {self.humor}')
         
     def mudar_humor(self, i):
         self.humor = self.humores[i]
+
+    def crescer(self):
+        self.idade += 1
