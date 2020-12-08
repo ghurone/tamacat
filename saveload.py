705427855d71c3f3a247a23c9fe2db56a288a8b6
Ghuron <ghuron@usp.br>
2020-12-08 14:09:21 -0300

    save and load obj

---
+++ saveload.py
@@ -0,0 +1,31 @@
+import pickle
+
+
+# noinspection PyTypeChecker
+def salvar_jogo(*objs):
+    try:
+        for i in range(len(objs)):
+            t = 'ab'
+            if i == 0:
+                t = 'wb'
+
+            with open('save.file', t) as file:
+                pickle.dump(objs[i], file)
+                file.write(bytes('\n'.encode('utf8')))
+
+    except Exception as e:
+        print(e)
+
+
+def carregar_jogo():
+    try:
+        with open('save.file', 'rb') as file:
+            objs = file.readlines()
+
+        list_objs = []
+        for obj in objs:
+            list_objs.append(pickle.loads(obj[:-1]))
+
+        return list_objs
+    except Exception as e:
+        print(e)
