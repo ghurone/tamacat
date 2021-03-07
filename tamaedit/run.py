0ad7a676834c0121b0662a6752cb36ecb354885e
Ghuron <ghuron@usp.br>
2021-03-07 02:25:38 -0300

    TAMAEDIT NO TERMINAL

--- tamaedit/run.py
+++ tamaedit/run.py
@@ -1,3 +1,8 @@
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from objs.gatinho import Adotado, Resgatado, Comprado
 from objs.brinquedo import *
 from objs.bau import Bau
