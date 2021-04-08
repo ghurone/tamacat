6590fc27453bcd34654f178423a67e15c65a7365
Ghuron <ghuron@usp.br>
2021-04-07 22:49:03 -0300

    animais das cartas

--- brincadeiras/memoria.py
+++ brincadeiras/memoria.py
@@ -1,15 +1,69 @@
 import config.janela as cjane
 
+animais = [['         ',
+            '  _   _  ',
+            ' (q\\_/p) ',
+            '  |. .|  ',
+            '  =\\,/=  ',
+            '         ',
+            '         '],
+
+           ['         ',
+            '    _\\_  ',
+            ' |\/  o\\ ',
+            ' |/\\___< ',
+            "    ''   ",
+            '         ',
+            '         '],
+
+           ['         ',
+            ' |\\---/| ',
+            ' | ° ° | ',
+            '  \\_T_/  ',
+            '         ',
+            '         ',
+            '         '],
+
+           ['         ',
+            ' /^---^\\ ',
+            ' V o o V ',
+            '  | Y |  ',
+            '   -.-   ',
+            '         ',
+            '         '],
+
+           ['         ',
+            '  ()-()  ',
+            "  (':')  ",
+            '  d . b  ',
+            '  ()_()  ',
+            '         ',
+            '         '],
+
+           ['         ',
+            '   ,_,   ',
+            '  (O,O)  ',
+            '  (   )  ',
+            '   " "   ',
+            '         ',
+            '         '],
+
+           [' .-",    ',
+            " `~||    ",
+            "   ||___ ",
+            "   (':.)`",
+            "   || || ",
+            "   || || ",
+            "   ^^ ^^ "]]
+
 
 def gerador(num):
-    num = '0' + str(num) if len(str(num)) == 1 else str(num)
-
     result = []
     for i in range(7):
         if i == 3:
-            result.append(f'{num}'.center(8))
+            result.append(f'{num}'.center(9))
         else:
-            result.append(' '*8)
+            result.append(' ' * 9)
 
     return result
 
@@ -21,26 +75,25 @@ def printar_jogo(lista):
     lin = 0
     for i in range(2, 21):
         if i == 2 or i == 10 or i == 12 or i == 20:
-            janela.muda_slice(i, 2, 79, ('+' + '-' * 8 + '+ ') * 7)
+            janela.muda_slice(i, 4, 75, ('+' + '-' * 9) * 7 + '+')
         elif i == 11:
             k = 7
             lin = 0
         else:
             conteudo = ''
             for j in range(7):
-                conteudo += '|' + lista[k+j][lin] + '| '
-
-            janela.muda_slice(i, 2, 79, conteudo)
+                conteudo += '|' + lista[k + j][lin]
+            conteudo += '|'
+            janela.muda_slice(i, 4, 75, conteudo)
             lin += 1
 
     print(janela)
 
 
 def jogar_memoria():
-    lista = [gerador(i) for i in range(1,15)]
+    lista = [gerador(i) for i in range(1, 15)]
+    #lista = animais + animais
     printar_jogo(lista)
     input('>>>')
 
-
-if __name__ == '__main__':
-    printar_jogo()
+jogar_memoria()
